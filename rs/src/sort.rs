1ea04563e5a315ab1a03275c33a20688fa17d1ea
Minh Do <minhdo@Minhs-MacBook-Pro.local>
2023-03-05 12:03:48 +0700

    Heap sort

--- rs/src/sort.rs
+++ rs/src/sort.rs
@@ -60,8 +60,20 @@ impl SortAlgorithm for BubbleSort {
 // strategy Heap Sort
 pub struct HeapSort;
 impl SortAlgorithm for HeapSort {
-  fn sort<T>(&self, arr: &mut [T]) {
-    println!("Heap sort ! #TODO");
+  fn sort<T: Ord>(&self, arr: &mut [T]) {
+    if arr.len() <= 1 {
+      return;
+  }
+
+  let last_parent = (arr.len() - 2) / 2;
+  for i in (0..=last_parent).rev() {
+      move_down(arr, i);
+  }
+
+  for end in (1..arr.len()).rev() {
+      arr.swap(0, end);
+      move_down(&mut arr[..end], 0);
+  }
   }
 }
 
@@ -79,4 +91,26 @@ impl Sort {
     pub fn sort<T: SortAlgorithm, K: Ord>(sort_alg: T, arr: &mut [K]) {
         sort_alg.sort(arr)
     }
+}
+
+
+fn move_down<T: Ord>(arr: &mut [T], mut root: usize) {
+  let last = arr.len() - 1;
+  loop {
+      let left = 2 * root + 1;
+      if left > last {
+          break;
+      }
+      let right = left + 1;
+      let child = if right <= last && arr[right] > arr[left] {
+          right
+      } else {
+          left
+      };
+
+      if arr[child] > arr[root] {
+          arr.swap(root, child);
+      }
+      root = child;
+  }
 }
