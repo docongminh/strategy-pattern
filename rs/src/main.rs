ebf17541b60c36c4d5b7df7ceed78bb56260bac9
Minh Do <minhdo@Minhs-MacBook-Pro.local>
2023-03-05 11:59:37 +0700

    Selection sort

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -3,5 +3,19 @@ pub mod sort;
 use crate::sort::*;
 
 fn main() {
-    println!("Hello, world!");
+    println!("----Sort numbers----");
+    
+    let mut arr = [4, 5, 2, -31, 0, 23, 8, 7, 1];
+    println!("Before: {:?}", arr);
+    
+    Sort::sort(SelectionSort, &mut arr);
+    println!("After:  {:?}\n", arr);
+
+    println!("----Sort strings----");
+    
+    let mut strings = ["money", "crypto", "stock", "car", "house", "gold"];
+    println!("Before: {:?}", strings);
+    
+    Sort::sort(SelectionSort, &mut strings);
+    println!("After:  {:?}\n", strings);
 }
--- rs/src/sort.rs
+++ rs/src/sort.rs
@@ -1,21 +1,29 @@
 pub trait SortAlgorithm {
-  fn sort(&self);
+  fn sort<T>(&self, arr: &mut[T]);
 }
-
 // strategy merge sort
 pub struct MergeSort;
 impl SortAlgorithm for MergeSort {
-  fn sort(&self) {
+  fn sort<T>(&self, arr: &mut[T]) {
     println!("merge sort ! #TODO")
   }
 }
 
-// strategy selection sort
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct SelectionSort;
 
 impl SortAlgorithm for SelectionSort {
-  fn sort(&self) {
-    println!("selection sort ! #TODO");
+  fn sort<T: Ord>(&self, arr: &mut [T]){
+    let len = arr.len();
+    for left in 0..len {
+        let mut smallest = left;
+        for right in (left + 1)..len {
+            if arr[right] < arr[smallest] {
+                smallest = right;
+            }
+        }
+        arr.swap(smallest, left);
+    }
   }
 }
 
@@ -23,7 +31,7 @@ impl SortAlgorithm for SelectionSort {
 pub struct QuickSort;
 
 impl SortAlgorithm for QuickSort {
-  fn sort(&self) {
+  fn sort<T>(&self, arr: &mut [T]) {
     println!("Quick sort ! # TODO");
   }
 }
@@ -31,7 +39,7 @@ impl SortAlgorithm for QuickSort {
 // strategy insertion sort
 pub struct InsertionSort;
 impl SortAlgorithm for InsertionSort {
-  fn sort(&self) {
+  fn sort<T>(&self, arr: &mut [T]) {
     println!("Insertion sort ! TODO");
   }
 }
@@ -39,7 +47,7 @@ impl SortAlgorithm for InsertionSort {
 // strategy bubble sort
 pub struct BubbleSort;
 impl SortAlgorithm for BubbleSort {
-  fn sort(&self) {
+  fn sort<T>(&self, arr: &mut [T]) {
     println!("bubble sort ! #TODO");
   }
 }
@@ -47,7 +55,7 @@ impl SortAlgorithm for BubbleSort {
 // strategy Heap Sort
 pub struct HeapSort;
 impl SortAlgorithm for HeapSort {
-  fn sort(&self) {
+  fn sort<T>(&self, arr: &mut [T]) {
     println!("Heap sort ! #TODO");
   }
 }
@@ -55,7 +63,15 @@ impl SortAlgorithm for HeapSort {
 // strategy Counting sort
 pub struct CountingSort;
 impl SortAlgorithm for CountingSort {
-  fn sort(&self) {
+  fn sort<T>(&self, arr: &mut [T]) {
     println!("Counting sort ! #TODO");
   }
+}
+
+
+pub struct Sort;
+impl Sort {
+    pub fn sort<T: SortAlgorithm, K: Ord>(sort_alg: T, arr: &mut [K]) {
+        sort_alg.sort(arr)
+    }
 }
